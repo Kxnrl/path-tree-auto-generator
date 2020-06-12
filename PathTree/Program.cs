94a62b4a264fd5884ad9da98d15e42d91f7607ea
Kyle <kyle@kxnrl.com>
2020-06-12 19:20:49 +0800

    Initial commit

---
+++ PathTree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PathTree
+{
+    class Program
+    {
+        static readonly string worker = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        static readonly string myself = Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        static readonly string myfile = "kxnrl.path.tree.txt";
+        static readonly string[] exts = new string[] {};
+
+        static void Main()
+        {
+            Console.Title = "目录树生成器   Path: [" + worker + "]";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Scanning...");
+
+            var files = new List<string>();
+            Directory.GetFileSystemEntries(worker, "*.*", SearchOption.AllDirectories)
+                .OrderBy(p => p).ToList().ForEach(f =>
+                {
+                    var fa = File.GetAttributes(f);
+                    if (fa.HasFlag(FileAttributes.Directory) || f.Contains(myfile) || f.Contains(myself))
+                        return;
+
+                    if (Array.IndexOf(exts, Path.GetExtension(f)) >= 0)
+                    {
+                        files.Add(f.Replace(worker, "").TrimStart('\\').Replace('\\', '/'));
+                    }
+                });
+            Console.WriteLine("Total: {0} files", files.Count);
+            Console.WriteLine("Writing...");
+
+            var list = Path.Combine(worker, myfile);
+            File.WriteAllLines(list, files.ToArray(), new UTF8Encoding(false));
+
+            Console.WriteLine("Wrote to {0}", list);
+            Console.WriteLine("Done...");
+            Console.ReadKey(true);
+        }
+    }
+}
